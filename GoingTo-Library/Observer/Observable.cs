f5e631c3ce0ff10bbbebdd483763c7fbc837839b
Jaime Alonso Garrido Guevara <alonso.garrido.2901@hotmail.com>
2020-06-26 14:49:17 -0500

    Observer interfaces

---
+++ GoingTo-Library/Observer/Observable.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace GoingTo_Library.Observer
+{
+    public interface IObservable
+    {
+        void RegisterObserver(IObserver Observer);
+        void RemoveObserver(IObserver Observer);
+        void NotifyObservers();
+    }
+}
