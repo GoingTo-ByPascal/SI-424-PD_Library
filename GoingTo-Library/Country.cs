46f411ebc8b97cb7ba647d3755efa732c0de2a57
IPorteniu <lporteniu@gmail.com>
2020-06-25 20:57:26 -0500

    Se crean las entidades

---
+++ GoingTo-Library/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoingTo_Library
+{
+    class Country
+    {
+        public int Id { get; set; }
+        public string ShortName { get; set; }
+        public string FullName { get; set; }
+        public Locatable Locatable { get; set; }
+        public int LocatableId { get; set; }
+    }
+}
