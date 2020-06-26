991da5615ff382b5e7d63ab34c014a287dbb4ca5
Jaime Alonso Garrido Guevara <alonso.garrido.2901@hotmail.com>
2020-06-25 21:04:40 -0500

    class creation

--- GoingTo-Library/Users.cs
+++ GoingTo-Library/Users.cs
@@ -4,5 +4,10 @@ namespace GoingTo_Library
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UserProfiles UserProfiles { get; set; }
+        
     }
 }
