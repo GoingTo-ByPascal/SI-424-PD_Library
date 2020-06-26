1c3877127705745a30d0fe48490cada2bdab1670
IPorteniu <lporteniu@gmail.com>
2020-06-26 00:07:58 -0500

    Se agrega patron state

---
+++ GoingTo-Library/State/PastTripState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoingTo_Library.State
+{
+    public class PastTripState : IState<Trip>
+    {
+        public void Future(Trip trip)
+        {
+            trip.Future();
+        }
+
+        public void Past(Trip trip)
+        {
+            throw new Exception("The trip is already in the past");
+        }
+    }
+}
---
+++ GoingTo-Library/State/State.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoingTo_Library
+{
+    public interface IState<T>
+    {
+        void Past(T trip);
+        void Future(T trip);
+    }
+}
