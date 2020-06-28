cff9fb13d0d0ba26eb5ec98d7f92886f2db69c01
Miguel Ugarte <miguel.cba11@gmail.com>
2020-06-28 02:38:51 -0500

    Façade

---
+++ GoingTo-Library/Façade/Interface/IFaçade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GoingTo_Library.Façade.Interface
+{
+    public interface IFaçade<T>
+    {
+        Task<T> ListAsync();
+        Task<T> ListByIdAsyn(int id);
+        Task<T> GetByIdAsync(int id);
+        Task<T> SaveAsync(T data);
+        Task<T> UpdateAsync(int id, T data);
+        Task<T> DeleteAsync(int id);
+        Task<T> AssignAsync(int firstId, int secondId);
+        Task<T> UnassignAsync(int firstId, int secondId);
+     }
+}
