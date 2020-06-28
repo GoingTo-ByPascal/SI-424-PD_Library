0bf33029666fc2443a23a3288c3da5b0512aa238
Miguel Ugarte <miguel.cba11@gmail.com>
2020-06-28 14:51:07 -0500

    singleton

---
+++ GoingTo-Library/Singleton/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace GoingTo_Library.Singleton
+{
+    public class Singleton<T>
+    {
+        private static Singleton<T> instance;
+        private Singleton()
+        {
+            instance = null;
+        }
+        public static Singleton<T> getInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton<T>();
+                return instance;
+            }
+            return instance;
+        }
+    }
+}
