826be859341167598fc588b94a056641f684f89a
IPorteniu <lporteniu@gmail.com>
2020-07-01 01:36:02 -0500

    Se crea patron factory

---
+++ GoingTo-Library/Factory/Travelable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoingTo_Library.Factory
+{
+    public abstract class Travelable
+    {
+        public abstract string StateName { get; set; }
+        public abstract IState<Travelable> CurrentState { get; set; }
+        
+    }
+}
---
+++ GoingTo-Library/Factory/TravelableFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoingTo_Library.Factory
+{
+    public abstract class TravelableFactory
+    {
+        public abstract Travelable CreateTravelable();
+        public abstract void SetState(IState<Travelable> state);
+    }
+}
